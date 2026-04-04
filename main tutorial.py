e4cb8292b0be4ba659ff10a82b649a98ad1e19b5
Lekitio <hanna.chen.citron@gmail.com>
2026-04-04 16:06:01 +0200

    I've organised player into a class/sprite

--- main tutorial.py
+++ main tutorial.py
@@ -14,14 +14,47 @@ pygame.display.set_caption("Game name")
 
 clock = pygame.time.Clock() #caps the framerate, Otherwise it runs infinitely fast
 
+def in_frame_checker(name, height, width):
+    if name.rect.bottom >= height:
+        name.rect.bottom = height
+        name.direction.y *= -1
+    if name.rect.top <= 0:
+        name.rect.top = 0 #gets rid of pesky bugs
+        name.direction.y *= -1
+    if name.rect.right >= width:
+        name.rect.right = width
+        name.direction.x *= -1        
+    if name.rect.left <= 0:
+        name.rect.left = 0
+        name.direction.x *= -1
 
 #making groups/classes/sprites
 class Player(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        #this is like refering that you have parents I guess?
+        #parent in this case is group
+        w = 50
+        h = 80
+        self.image = pygame.Surface((w, h))
+        self.rect = self.image.get_frect(topleft = (10,10))
+        self.direction = pygame.math.Vector2(1, -1) #2d
+        self.speed = 300
 
+    def update(self):
+        #update() will run this
+        keys = pygame.key.get_pressed() #this is a dictionary of all the keys
+        self.direction.x = keys[pygame.K_RIGHT]-keys[pygame.K_LEFT] #this jsut turns True -> 1, crazy
+        player.rect.center += player.direction * player.speed * delta_time #can control speed
+        in_frame_checker(player, height, width) #makes sure its in frame
+all_sprites = pygame.sprite.Group()
+#holds and manages all sprites, grand master
 
+player = Player(all_sprites)
 
 grey = (71, 68, 72)
-#colors the screen
+#colors the screen main_display.fill(grey). But i wanna do it constantly so into the game
+
 
 floor_color = (45, 35, 46)
 # pygame.FRect((0, 520), (900, 300))
@@ -35,15 +68,15 @@ if you_wanna_learn:
 
 #Making the player
 
-player_color = (241, 240, 234)
-w = 50
-h = 80
-player = pygame.Surface((w, h))
+# player_color = (241, 240, 234) #not used
+# w = 50
+# h = 80
+# player = pygame.Surface((w, h))
 
-#another use of rectangle is knowing player pos
-player_rect = player.get_frect(topleft = (10,10))
-player_direction = pygame.math.Vector2(1, -1) #2d
-player_speed = 300
+# #another use of rectangle is knowing player pos
+# # player_rect = player.get_frect(topleft = (10,10))
+# # player_direction = pygame.math.Vector2(1, -1) #2d
+# # player_speed = 300
 
 #load images FUTURE FIX
 # bg = pygame.image.load("C:\Users\Hanna\Pictures\Screenshots\Screenshot2026-01-17223534.png")
@@ -61,6 +94,9 @@ while running:
         #to make sure everyone moves the same speed and not make one persons game much faster cause they got more framse
         # -> math and delta_time (how long time (ms) to render one frame)
 
+    #getting all sprites up to date
+    all_sprites.update()
+
     #making display
     if you_wanna_learn:
         pass
@@ -68,22 +104,21 @@ while running:
         #pygame.mouse
     main_display.fill(grey)
     ground = pygame.draw.rect(main_display, floor_color, [0, 400, 900, 300]) #rect has other benefits too
-    main_display.blit(player, player_rect) #this is our player
-    player_rect.center += player_direction * player_speed * delta_time #can control speed
+    # main_display.blit(player, player_rect) #this is our player
 
     #check if in frame?
-    if player_rect.bottom >= height:
-        player_rect.bottom = height
-        player_direction.y *= -1
-    if player_rect.top <= 0:
-        player_rect.top = 0 #gets rid of pesky bugs
-        player_direction.y *= -1
-    if player_rect.right >= width:
-        player_rect.right = width
-        player_direction.x *= -1        
-    if player_rect.left <= 0:
-        player_rect.left = 0
-        player_direction.x *= -1
+    # if player.rect.bottom >= height:
+    #     player.rect.bottom = height
+    #     player.direction.y *= -1
+    # if player.rect.top <= 0:
+    #     player.rect.top = 0 #gets rid of pesky bugs
+    #     player.direction.y *= -1
+    # if player.rect.right >= width:
+    #     player.rect.right = width
+    #     player.direction.x *= -1        
+    # if player.rect.left <= 0:
+    #     player.rect.left = 0
+    #     player.direction.x *= -1
 
     #event loop
     if you_wanna_learn: #info about event actions
@@ -117,8 +152,8 @@ while running:
         
         #if you wanna go up and down too diagonals need this fix
         # player_rect.center += player_direction.normalize() if player_direction else player_direction
-    keys = pygame.key.get_pressed() #this is a dictionary of all the keys
-    player_direction.x = keys[pygame.K_RIGHT]-keys[pygame.K_LEFT] #this jsut turns True -> 1, crazy
+    # keys = pygame.key.get_pressed() #this is a dictionary of all the keys
+    # player_direction.x = keys[pygame.K_RIGHT]-keys[pygame.K_LEFT] #this jsut turns True -> 1, crazy
     # if keys[pygame.K_RIGHT]:
     #     player_direction.x = 1
     # else:
@@ -127,6 +162,14 @@ while running:
     recent_keys = pygame.key.get_just_pressed()
     if recent_keys[pygame.K_SPACE]:
         print("pew")
+
+
+    all_sprites.draw(main_display)
+    if you_wanna_learn:
+        pass
+        #.update() - updates every spite, inbyggd in sprite to appear or something
+        #.draw() blit the sprite images
+        #gets everything in the group to appear i guess
 
     pygame.display.update()
     #updates background every loop
