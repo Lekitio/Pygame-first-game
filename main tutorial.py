a6ad99d597a78541a1fa57602642fdef781aaf6f
Lekitio <hanna.chen.citron@gmail.com>
2026-04-05 18:43:51 +0200

    Realized I was calling self.rect.centery += self.velocity.y * delta_time twice in the code. Debugged and removed the one in the jump() method and adjusted the jump constants to recalibrate the jumping vibe

--- main tutorial.py
+++ main tutorial.py
@@ -2,74 +2,64 @@ import pygame
 from os.path import join
 from random import randint
 
-pygame.init()
-#"gets pygame started up"
 
-## Pygame display info: https://www.pygame.org/docs/ref/display.html#pygame.display.init
-WIDTH = 900
-HEIGHT = 600
-main_display = pygame.display.set_mode((WIDTH, HEIGHT)) 
-#this creates a screen, set_mode specifically
-
-pygame.display.set_caption("Game name")
-#this gives the window a name
-
-#importing all images
-japanese_pillar_1_surf = pygame.image.load(join("images", "Japanese_rock_pillar1.png")).convert_alpha()
-fog_nr_1_surf = pygame.image.load(join("images", "fog_nr_1.png")).convert_alpha()
-font = pygame.font.Font(None, 50)
-text_surf = font.render("Text", True, "red")
-
-clock = pygame.time.Clock() #caps the framerate, Otherwise it runs infinitely fast
-
-
-GRAVITY = 700
-
-def in_frame_checker(name, HEIGHT, WIDTH):
-    if name.rect.bottom >= HEIGHT:
-        name.rect.bottom = HEIGHT
-    if name.rect.top <= 0:
-        name.rect.top = 0 #gets rid of pesky bugs
-    if name.rect.right >= WIDTH:
-        name.rect.right = WIDTH
-    if name.rect.left <= 0:
-        name.rect.left = 0
-    
 #making groups/classes/sprites
 class Player(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
         #this is like refering that you have parents I guess?
         #parent in this case is group
+
+        self.init_display()
+        self.init_movement()
+        self.init_jump()
+        self.init_slash()
+
+    #clean init
+    def init_movement(self):
+        self.speed = 300
+        self.velocity = pygame.math.Vector2(1, 1)
+
+    def init_display(self):
         w = 40
         h = 60
         self.image = pygame.Surface((w, h))
         self.rect = self.image.get_frect(bottomleft = (10, ground.top))
-        # self.velocity = pygame.math.Vector2(1, 0) #defult
-        self.speed = 300
-
-        #Physics
-        self.velocity = pygame.math.Vector2(1, 1)
-        #Jumping physics
-        self.on_ground = True
-        self.jump_force = 400
-
-        #Jump
-        self.can_jump = True
-        self.last_jump_time = 0
-        self.jump_cooldown = 100
-        self.current_jump_time = 0
-        self.landed = False #this isnt optimal
-        self.jump_key = pygame.K_z
-        self.jump_speed_cap = 600
-        self.jump_velocity_multiplier = 0.3
-        self.groundy = ground.top
-
-        #Slash
+        
+    def init_slash(self):
         self.can_slash = True
         self.last_slash_time = 0
         self.cooldown_slash = 1000
 
+    def init_jump(self):
+        #variables ig?
+        self.can_jump = True
+        self.current_jump_time = 0
+        self.last_jump_time = 0
+        self.on_ground = True
+
+        #constants
+        self.jump_force = 800
+        self.jump_key = pygame.K_z
+        self.jump_cooldown = 100
+        self.jump_speed_cap = 600
+        self.jump_velocity_multiplier = 0.6
+
+        #pos
+        self.groundy = ground.top
+
+    #methods
+    def in_frame_checker(self, HEIGHT, WIDTH):
+        # @staticmethod uh i forgot how to use this nvm i dont need anymore
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+        if self.rect.top <= 0:
+            self.rect.top = 0 #gets rid of pesky bugs
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left <= 0:
+            self.rect.left = 0
+        
     def can_jump_cooldown(self):
         if not self.can_jump:
             self.current_jump_time = pygame.time.get_ticks()
@@ -90,24 +80,23 @@ class Player(pygame.sprite.Sprite):
             
         #if släppa and upwards
         if not keys[self.jump_key] and self.velocity.y < 0:
-            self.velocity.y *= 0.05
+            self.velocity.y *= 0.1
 
         # a speed cap
         self.velocity.y = min(self.jump_speed_cap, self.velocity.y)
 
-        #set actions into place
-        self.rect.centery += self.velocity.y * delta_time
-
-        if self.rect.bottom >= self.groundy:
+    def jump_collision_control(self):
+        if self.rect.bottom > self.groundy:
+            #Reason for this being below move and > rather than >= is because otherwise i will be below the ground when jumping -> if will be true -> fell directly -> no jumping. 
             if self.on_ground == False:
                 self.last_jump_time = pygame.time.get_ticks()
             self.rect.bottom = self.groundy
             self.on_ground = True
-            self.can_jump = False
             self.velocity.y = 0
-
+            #Timer 
+            self.can_jump = False
         self.can_jump_cooldown()
-
+    
     def slash_timer(self):
         if not self.can_slash:
             current_slash_time = pygame.time.get_ticks()
@@ -117,14 +106,21 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         #update() will run this
         keys = pygame.key.get_pressed() #this is a dictionary of all the keys
+        
         self.velocity.x = keys[pygame.K_RIGHT]-keys[pygame.K_LEFT] #this jsut turns True -> 1, crazy
-        self.rect.centerx += self.velocity.x * self.speed * delta_time #can control speed
-        self.rect.centery += self.velocity.y * delta_time
-        in_frame_checker(player, HEIGHT, WIDTH) #makes sure its in frame
-
+        
         #Getting inputs
         # jumping mechanism
         self.jump(keys, delta_time)
+        
+        self.rect.centerx += self.velocity.x * self.speed * delta_time #can control speed
+        self.rect.centery += self.velocity.y * delta_time #confsuion, i have one in jump too
+
+        #collision controll + jump cooldown
+        self.jump_collision_control()
+
+        print(self.velocity.y, delta_time)
+        self.in_frame_checker(HEIGHT, WIDTH) #makes sure its in frame
 
         recent_keys = pygame.key.get_just_pressed()
         if recent_keys[pygame.K_SPACE] and self.can_slash:
@@ -149,24 +145,12 @@ class Object1(GameObjects):
     def __init__(self, image, groups):
         pos = (randint(0, WIDTH), ground.top + 30)
         super().__init__(image, pos, groups, scale=(100, 150))
-        #question if you grandparent gets updated then do you also update
-        # self.image = pygame.image.load(join("images", "Japanese_rock_pillar1.png")).convert_alpha()
-        #import every time, inefficient time
-        
-
-        # self.image = pygame.transform.scale(image, (100, 150))
-        #bahahahhahaha you can squish the import
-        # self.rect = self.image.get_bounding_rect() This is what i should use honestly OR just fix the art from the beginning
-        # self.rect = self.image.get_frect(midbottom = (randint(0, WIDTH), ground.top + 30))
-        # its currently transparent so i need to move it down, hardcoded
+        #question if you grandparent gets updated then do you also update # self.image = pygame.image.load(join("images", "Japanese_rock_pillar1.png")).convert_alpha() #import every time, inefficient time # self.image = pygame.transform.scale(image, (100, 150)) #bahahahhahaha you can squish the import # self.rect = self.image.get_bounding_rect() This is what i should use honestly OR just fix the art from the beginning # self.rect = self.image.get_frect(midbottom = (randint(0, WIDTH), ground.top + 30)) # its currently transparent so i need to move it down, hardcoded
 
 class Fog(GameObjects): #Since its so similar i can jst call to a parent class
     def __init__(self, image, groups):
-        pos = (randint(0, WIDTH), randint(100, ground.top +30))
-        super().__init__(image, pos, groups, scale_by=0.1) #refers to parent
-        # self.image = image
-        # self.image = pygame.transform.scale_by(self.image, 0.1)
-        # self.rect = self.image.get_frect(midbottom = (randint(0, WIDTH), randint(100, ground.top +30)))
+        pos = (randint(0, WIDTH), randint(100, ground.top + 30))
+        super().__init__(image, pos, groups, scale_by=0.1) #calling parent
         
         self.velocity = pygame.math.Vector2(1, 0)
         self.speed = randint(1, 5)
@@ -184,25 +168,49 @@ def collision_pillar(sprite_group):
         pygame.draw.rect(main_display, floor_color, pygame.FRect(pillar.rect.centerx-10, pillar.rect.top-30, 30, 30))
 
 
-all_sprites = pygame.sprite.Group()
-#holds and manages all sprites, grandparent
+pygame.init()
+#"gets pygame started up"
 
+#initial display
+## Pygame display info: https://www.pygame.org/docs/ref/display.html#pygame.display.init
+WIDTH, HEIGHT = 900, 600
+main_display = pygame.display.set_mode((WIDTH, HEIGHT)) 
+#this creates a screen, set_mode specifically
+
+pygame.display.set_caption("Game name")
+#this gives the window a name
+
+#importing all images
+japanese_pillar_1_surf = pygame.image.load(join("images", "Japanese_rock_pillar1.png")).convert_alpha()
+fog_nr_1_surf = pygame.image.load(join("images", "fog_nr_1.png")).convert_alpha()
+font = pygame.font.Font(join("images", "times.ttf"), 50)
+text_surf = font.render("Text", True, "red")
+
+clock = pygame.time.Clock() #caps the framerate, Otherwise it runs infinitely fast
+
+#constant
+GRAVITY = 1300
+#colors
 grey = (71, 68, 72) #colors the screen main_display.fill(grey). But i wanna do it constantly so into the game
-
-#Makes the floor rectangle
 floor_color = (45, 35, 46)
+
+#displays and blocks and surfces
+#Makes the floor rectangle
 ground = pygame.Rect(0, 400, 900, 500)
 
-#creating objects, order matter.
-#   created first -> gets covered. 
+#sprite creating&grouping
+all_sprites = pygame.sprite.Group()#holds and manages all sprites, grandparent/super()
 japanese_pillar_1_sprites = pygame.sprite.Group() #more organised, usually for usually gonna get acessed but not as often as ex, player. Stuff
+all_fog_sprites = pygame.sprite.Group()
+
+#creating objects, order matter. created first -> gets covered. 
 for i in range(10):
     Object1(japanese_pillar_1_surf, (all_sprites, japanese_pillar_1_sprites))
 player = Player(all_sprites)
 
-all_fog_sprites = pygame.sprite.Group()
 for i in range(10):
     Fog(fog_nr_1_surf, (all_sprites, all_fog_sprites))
+
 
 # pygame.FRect((0, 520), (900, 300))
 #FRects are rectangles but with decimals -> more precise #Rectangles can give you acess to a bunch of useful points of the rectangle -> less math #for storing coordinates
@@ -233,8 +241,9 @@ while running:
     all_sprites.draw(main_display)
 #.update() - updates every spite, inbyggd in sprite to appear or something #.draw() blit the sprite images #gets everything in the group to appear i guess
     collision_pillar(japanese_pillar_1_sprites)
-
+    # pygame.display.draw(main_display, font)
     # main_display.blit(fog_1)
+    main_display.blit(text_surf)
     pygame.display.update()
     #updates background every loop
 
